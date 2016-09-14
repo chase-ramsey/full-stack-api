fa2466af7b7b8eeb77c6624b9279bde28e4e34a5
Chase Ramsey <ramsey.chase@gmail.com>
2016-09-14 11:37:23 -0500

    Created basic models

--- reviews_api/models.py
+++ reviews_api/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Media(models.Model):
+  media_type = models.ChoiceField('album', 'book', 'game', 'movie', 'show')
+  title = models.CharField(max_length=100)
+  creator = models.CharField(max_length=100)
+  yearReleased = models.IntegerField()
+
+  def __str__(self):
+    return self.title
+
+
+class Review(models.Model):
+  media = models.ForeignKey(Media, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  full_text = models.TextField()
+  processed_text = models.TextField()
+
+
+class Tag(models.Model):
+  word = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.word
+
+
+class ReviewTag(models.Model):
+  review = models.ForeignKey(Review, on_delete=models.CASCADE)
+  tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+class List(models.Model):
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class ListReview(models.Model):
+  list = models.ForeignKey(List, on_delete=models.CASCADE)
+  review = models.ForeignKey(Review, on_delete=models.CASCADE)
--- reviews_api/views.py
+++ reviews_api/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from reviews_api.models import Media, Review, Tag, ReviewTag, List, ReviewList
+from reviews_api.serializers import ReviewSerializer
+from rest_framework import mixins
+from rest_framework import generics
 
 # Create your views here.
